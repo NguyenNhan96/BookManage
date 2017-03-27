b59c0512890bec54aab4079c5fa746a68fdfd141
NhanNguyen <nhankendu96@gmail.com>
2017-03-27 21:29:42 +0700

    Đây..File cập nhật mới nhất.

--- DoAnFileCu/QLKHOSACH/Form/QTCSDL/Program.cs
+++ DoAnFileCu/QLKHOSACH/Form/QTCSDL/Program.cs
@@ -16,7 +16,7 @@ namespace QTCSDL
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new DonXuat());
+            Application.Run(new DonNhap());
         }
     }
 }
